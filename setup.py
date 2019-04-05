2eacc0e9b48dc7ed674a592caa34b60cba1f6035
chrzyki <christoph@functor-argument.net>
2019-04-05 09:55:33 +0200

    Increased pylexibank version.

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ with open('metadata.json', **({} if PY2 else {'encoding': 'utf-8'})) as fp:
 
 setup(
     name='lexibank_halenepal',
-    version='1.0',
     description=metadata['title'],
     license=metadata.get('license', ''),
     url=metadata.get('url', ''),
@@ -23,7 +22,7 @@ setup(
         ]
     },
     install_requires=[
-        'pylexibank>=0.13.1',
+        'pylexibank>=1.0.0',
     ],
     extras_require={
         'test': [
