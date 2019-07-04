c511932cec3bdc186d5a90f6d35f3d0298025c23
Christoph Rzymski <christoph@functor-argument.net>
2019-07-04 10:37:41 +0200

    Code cleanup, Glottolog 4.0, release 1.0.1.

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Conceptlists in Concepticon:
 
 ## Statistics
 
+
 [![Build Status](https://travis-ci.org/lexibank/halenepal.svg?branch=master)](https://travis-ci.org/lexibank/halenepal)
 ![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
 ![Concepticon: 69%](https://img.shields.io/badge/Concepticon-69%25-orange.svg "Concepticon: 69%")
@@ -24,4 +25,3 @@ Conceptlists in Concepticon:
 - **Tokens:** 69,365
 - **Segments:** 158 (0 BIPA errors, 0 CTLS sound class errors, 158 CLTS modified)
 - **Inventory size (avg):** 69.31
-
--- lexibank_halenepal.py
+++ lexibank_halenepal.py
@@ -1,21 +1,15 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
+import re
+from collections import namedtuple
 
 from clldutils.misc import slug
 from clldutils.path import Path
-from pylexibank.dataset import Dataset as NonSplittingDataset
 from clldutils.text import strip_brackets, split_text
 from csvw import Datatype
-
+from pylexibank.dataset import Dataset as NonSplittingDataset
 from tqdm import tqdm
-import re
 
-from collections import namedtuple
-
-# moving this to avoid extra library code
 STEDT = namedtuple(
-    "STEDT",
-    ["rn", "reflex", "gloss", "gfn", "srcabbr", "lgid", "language", "srcid"],
+    "STEDT", ["rn", "reflex", "gloss", "gfn", "srcabbr", "lgid", "language", "srcid"]
 )
 Hale = namedtuple("Hale", ["id", "gloss", "srcid"])
 
@@ -28,39 +22,29 @@ class Dataset(NonSplittingDataset):
         pass
 
     def clean_form(self, item, form):
-        if form not in ['*', '---', '-']:
-            form = strip_brackets(split_text(form, separators=';,/')[0])
+        if form not in ["*", "---", "-"]:
+            form = strip_brackets(split_text(form, separators=";,/")[0])
             return form
 
     def cmd_install(self, **kw):
         hale, stedt = [], []
         languages, concepts = {}, {}
 
-        reps = [
-                ('XIII', '13'),
-                ('XII', '12'),
-                ('A', 'a.'),
-                ('B', 'b.'),
-                ('C', 'c.'),
-                ('D', 'd.')
-                ]
+        reps = [("XIII", "13"), ("XII", "12"), ("A", "a."), ("B", "b."), ("C", "c."), ("D", "d.")]
 
         mapper = {
-            '01.009': '01.009', 
-            'XIIC56': '12c.56', 
-            '01.009': '01.009', 
-            'XIIC64': '12c.64', 
-            'XIID31': '12d.31', 
-            'XIID14': '12d.14', 
-            '01.009': '01.009',
-            }
+            "01.009": "01.009",
+            "XIIC56": "12c.56",
+            "XIIC64": "12c.64",
+            "XIID31": "12d.31",
+            "XIID14": "12d.14",
+        }
 
         # corrected srcids
         fromconcepts = {}
         for element in self.raw.read_tsv(self.raw / "srcids.tsv"):
             if element:
                 fromconcepts[element[-1]] = element[-2]
-
 
         hsrcids = set()
         for element in self.raw.read_tsv(self.raw / "Hale_raw.tsv"):
@@ -75,9 +59,9 @@ class Dataset(NonSplittingDataset):
                 element[-1] = srcid
                 if srcid in hsrcids:
                     stedt.append(STEDT(*element))
-                
+
                 else:
-                    for elm in re.split('[,;]', element[-1]): #.split(';'):
+                    for elm in re.split("[,;]", element[-1]):  # .split(';'):
                         for s, t in reps:
                             elm = elm.replace(s, t)
                         elm = mapper.get(elm, elm)
@@ -86,36 +70,34 @@ class Dataset(NonSplittingDataset):
                                 missing.add((elm, element[-1], element[2]))
                             else:
                                 mapper[element[-1]] = elm
-                                stedt.append(STEDT(*[x for x in
-                                    element[:-1]]+[elm]))
+                                stedt.append(STEDT(*[x for x in element[:-1]] + [elm]))
         for i, y in enumerate(missing):
-            print('{0:5} | {1:10} | {2:10} | {3}'.format(
-                i+1, y[0],y[1], y[2]))
-
+            print("{0:5} | {1:10} | {2:10} | {3}".format(i + 1, y[0], y[1], y[2]))
 
         with self.cldf as ds:
-            self.cldf.tokenize = lambda x, y: ' '.join(self.tokenizer(x, '^'+y+'$',
-                    column='IPA')).split(' + ')
-            
+            self.cldf.tokenize = lambda x, z: " ".join(
+                self.tokenizer(x, "^" + z + "$", column="IPA")
+            ).split(" + ")
+
             # add data to cldf
-            ds['FormTable', 'Segments'].separator = ' + '
-            ds['FormTable', 'Segments'].datatype = Datatype.fromvalue({
-                "base": "string",
-                "format": "([\\S]+)( [\\S]+)*"
-            })
+            ds["FormTable", "Segments"].separator = " + "
+            ds["FormTable", "Segments"].datatype = Datatype.fromvalue(
+                {"base": "string", "format": "([\\S]+)( [\\S]+)*"}
+            )
 
             ds.add_sources(*self.raw.read_bib())
-            ds.add_concepts(id_factory=lambda c: c.id)
-            #for concept in self.concepts:
-            #    ds.add_concept(
-            #        ID=concept["ID"],
-            #        Name=concept["GLOSS"],
-            #        Concepticon_ID=concept["CONCEPTICON_ID"],
-            #        Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
-            #    )
-            concepts = {concept.english: concept.id for concept in
-                    self.conceptlist.concepts.values()}
-            #    concepts[concept["GLOSS"]] = concept["ID"]
+
+            for concept in self.concepts:
+                ds.add_concept(
+                    ID=concept["ID"],
+                    Name=concept["ENGLISH"],
+                    Concepticon_ID=concept["CONCEPTICON_ID"],
+                    Concepticon_Gloss=concept["CONCEPTICON_GLOSS"],
+                )
+
+            concepts = {
+                concept.english: concept.id for concept in self.conceptlist.concepts.values()
+            }
 
             for language in self.languages:
                 ds.add_language(
@@ -124,14 +106,15 @@ class Dataset(NonSplittingDataset):
                     Name=language["LANGUAGE"],
                 )
                 languages[language["LANGUAGE"]] = slug(language["GLOTTOLOG"])
-            
+
             for h in hale:
                 concepts[h.srcid] = h.id
+
             for entry in tqdm(stedt):
                 ds.add_lexemes(
-                        Local_ID=entry.rn,
-                        Language_ID=languages[entry.language],
-                        Parameter_ID=concepts[entry.srcid],
-                        Value=entry.reflex,
-                        Source=['Hale1973']
-                        )
+                    Local_ID=entry.rn,
+                    Language_ID=languages[entry.language],
+                    Parameter_ID=concepts[entry.srcid],
+                    Value=entry.reflex,
+                    Source=["Hale1973"],
+                )
