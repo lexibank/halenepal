02a78b5e4509bcd5c26a6dfe40450cfb950c81f6
chrzyki <christoph@functor-argument.net>
2019-11-07 09:09:56 +0100

    Added tests, updated setup py

--- setup.py
+++ setup.py
@@ -2,30 +2,19 @@ from setuptools import setup
 import json
 
 
-with open('metadata.json') as fp:
+with open("metadata.json") as fp:
     metadata = json.load(fp)
 
 
 setup(
-    name='lexibank_halenepal',
-    description=metadata['title'],
-    license=metadata.get('license', ''),
-    url=metadata.get('url', ''),
-    py_modules=['lexibank_halenepal'],
+    name="lexibank_halenepal",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_halenepal"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={
-        'lexibank.dataset': [
-            'halenepal=lexibank_halenepal:Dataset',
-        ]
-    },
-    install_requires=[
-        'pylexibank>=1.1.1',
-    ],
-    extras_require={
-        'test': [
-            'pytest-cldf',
-        ],
-    },
+    entry_points={"lexibank.dataset": ["halenepal=lexibank_halenepal:Dataset"]},
+    install_requires=["pylexibank>=2.1"],
+    extras_require={"test": ["pytest-cldf"]},
 )
-
