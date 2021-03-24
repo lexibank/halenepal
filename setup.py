8ed928aaab1e2fc3f44fec7ee3822d26aae30128
Christoph Rzymski <christoph@functor-argument.net>
2021-03-24 11:41:45 +0100

    Re-generate with updated pylexibank and clts

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     entry_points={"lexibank.dataset": ["halenepal=lexibank_halenepal:Dataset"]},
-    install_requires=["pylexibank>=2.1"],
+    install_requires=["pylexibank>=3.0.0"],
     extras_require={"test": ["pytest-cldf"]},
 )
